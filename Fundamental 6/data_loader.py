f012b4be23a63b659d020ec1806e6c1f6a6594eb
Sergio Jaziel Butzmann Martinez <97535091+Xraight@users.noreply.github.com>
2025-11-21 21:39:17 -0600

    Fix file path in data loading function

--- Fundamental 6/data_loader.py
+++ Fundamental 6/data_loader.py
@@ -3,9 +3,10 @@ import pandas as pd
 def cargar_datos(ruta_archivo):
     """Carga el CSV y devuelve un DataFrame."""
     try:
-        df = pd.read_csv("high_diamond_ranked_10min.csv")
+        df = pd.read_csv(ruta_archivo)
         print(f"✅ Datos cargados correctamente: {df.shape[0]} partidas encontradas.")
         return df
     except FileNotFoundError:
         print(f"❌ Error: No se encontró el archivo '{ruta_archivo}'.")
+
         return None
